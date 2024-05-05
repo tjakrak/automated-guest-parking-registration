50223a5f636f19feffd365a5e03eeac937266ba4
ryantjakrakartadinata <rgtjakrakartadinata@dons.usfca.edu>
2024-05-04 19:53:20 -0700

    added some delay before clicking the link

--- parking_registration.py
+++ parking_registration.py
@@ -17,7 +17,8 @@ chrome_options = Options()
 chrome_options.add_argument("--verbose")
 chrome_options.add_argument("--headless") # Ensure GUI is off
 chrome_options.add_argument("--no-sandbox") # Bypass OS security model
-chrome_options.add_argument("--disable-dev-shm-usage") # Overcome limited resource problems
+# chrome_options.add_argument("--disable-dev-shm-usage") # Overcome limited resource problems
+chrome_options.add_argument("--window-size=1440 x 900")
 
 inputs = {}
 # List of input data corresponding to each field's name attribute
